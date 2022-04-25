370f0cf2c50d1f3d1d6ea35dd66f89a1ed9bc478
vuphan0804 <vuphan0804@gmail.com>
2022-04-25 22:19:09 +0700

    Complete Exercise 2

---
+++ TinhTienDien.js
@@ -0,0 +1,44 @@
+function totalMoneyUseElectric() {
+  var nameCustomer = document.getElementById("nameCustomer").value;
+  var kwUse = document.getElementById("kwUse").value;
+  var totalMoneyFinal;
+
+  function calcMoney() {
+    document.getElementById("showResult").style = "display:block; color:green";
+    document.getElementById(
+      "totalMoney"
+    ).innerHTML = `Họ tên ${nameCustomer}. Tiền điện ${totalMoneyFinal} vnd`;
+  }
+  function errorText() {
+    document.getElementById("showResult").style = "block; color:red";
+    document.getElementById(
+      "totalMoney"
+    ).innerHTML = `Nhập không hợp lệ. Vui lòng nhập lại!`;
+  }
+
+  if (kwUse <= 0) {
+    errorText();
+  } else if (kwUse > 0 && kwUse <= 50) {
+    totalMoneyFinal = kwUse * 500;
+    calcMoney();
+  } else if (kwUse > 50 && kwUse <= 100) {
+    totalMoneyFinal = 50 * 500 + (kwUse - 50) * 650;
+    calcMoney();
+  } else if (kwUse > 100 && kwUse <= 200) {
+    totalMoneyFinal = 50 * 500 + 50 * 650 + (kwUse - 100) * 850;
+    calcMoney();
+  } else if (kwUse > 200 && kwUse <= 350) {
+    totalMoneyFinal = 50 * 500 + 50 * 650 + 100 * 850 + (kwUse - 200) * 1100;
+    calcMoney();
+  } else {
+    totalMoneyFinal =
+      50 * 500 + 50 * 650 + 100 * 850 + 150 * 1100 + (kwUse - 350) * 1300;
+    calcMoney();
+  }
+
+  //   document.getElementById("showResult").style = "block";
+  //   document.getElementById(
+  //     "totalMoney"
+  //   ).innerHTML = `Họ tên ${nameCustomer}. Tiền điện ${totalMoneyFinal} vnd`;
+  //
+}
